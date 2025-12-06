4793ad68a7e0e6452a2492251849049748bb0a5d
Romario Cohen <romariocohen98@gmaail.com>
2025-12-06 17:30:42 -0500

    Fix VITE_API_BASE_URL and remove .env

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  define: {
+    __APP_API_URL__: JSON.stringify(process.env.VITE_API_BASE_URL),
+  }
+});
 
 
